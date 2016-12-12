b3c8a63ff6a5fa4776c0d100ecdedcaa3bae66f0
Justin Roche <jproche5@gmail.com>
2016-12-11 19:18:55 -0500

    displays fbContacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,11 @@ import { Store } from '../shared/store';
         <a *ngIf="authenticated() && hasBot()" routerLink="/manage" routerLinkActive="active">Manage Bots</a>
         <a *ngIf="authenticated()" routerLink="/view-activities" routerLinkActive="active">View Activities</a>
         <a *ngIf="authenticated()" routerLink="/logout" routerLinkActive="active" class="right">Logout</a>
-        <a class='login' *ngIf="!authenticated()" class="right" (click)="login()">Login</a>
+        
     </nav>
     <router-outlet></router-outlet>
   `,
-
+// <a class='login' *ngIf="!authenticated()" class="right" (click)="login()">Login</a>
 })
 
 
